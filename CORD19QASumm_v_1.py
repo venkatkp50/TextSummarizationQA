4d3b23f43bb75ce5fbc8c2d2eb29f6215e2341eb
venkatkp50 <95966437+venkatkp50@users.noreply.github.com>
2023-04-07 16:59:20 +0530

    Update CORD19QASumm_v_1.py

--- CORD19QASumm_v_1.py
+++ CORD19QASumm_v_1.py
@@ -301,8 +301,9 @@ if user_message != '':
             st.write('else .........')
             for line in full_text:
                 para.append(line) 
+            st.write('else  .........  ...para len',len(para))
             berttext = ''.join( lines for lines in para) 
-            st.write('else ......... in para max_sent_size=',max_sent_size)
+            st.write('else ......... in para max_sent_size=',max_sent_size,'...para len',len(para))
             bert_model = Summarizer()
             st.write('bert_model instantised  ..............')
             berttext = bert_model(berttext,max_length=max_abstract_token_size,num_sentences=max_sent_size)
