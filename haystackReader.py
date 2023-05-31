0dadc6afde3f5e2b86f6c3f764dfc95aab052237
venkatkp50 <95966437+venkatkp50@users.noreply.github.com>
2023-05-31 14:50:04 +0530

    Update haystackReader.py

--- haystackReader.py
+++ haystackReader.py
@@ -3,11 +3,11 @@ from haystack.utils import clean_wiki_text
 from haystack.utils import convert_files_to_docs
 from haystack.utils import fetch_archive_from_http,print_answers
 from haystack.document_stores import InMemoryDocumentStore
-from haystack.document_stores import ElasticsearchDocumentStore
-from haystack.pipelines.standard_pipelines import TextIndexingPipeline
+#from haystack.document_stores import ElasticsearchDocumentStore
+#from haystack.pipelines.standard_pipelines import TextIndexingPipeline
 from haystack.nodes import BM25Retriever
 from haystack.nodes import FARMReader
-from haystack.pipelines import ExtractiveQAPipeline
+#from haystack.pipelines import ExtractiveQAPipeline
 import streamlit as st
 
 def getReaderResult(doc_dir,modelSelected,user_message):
