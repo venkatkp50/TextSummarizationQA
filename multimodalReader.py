aa743ca39cd7db0153fef1bc647de62dfa566c02
venkatkp50 <95966437+venkatkp50@users.noreply.github.com>
2023-06-21 13:49:47 +0530

    Update multimodalReader.py

--- multimodalReader.py
+++ multimodalReader.py
@@ -8,17 +8,16 @@ import torch
 
 
 
-#tutorial_running(19)
-
-def getImage(query):
-    print(query,'........................')
+@st.cache
+def getImageSessionpipeline():
+    print(query,'........................getImageSession')
     sent_trans = 'sentence-transformers/nli-bert-base'
     document_store = InMemoryDocumentStore(embedding_dim=512)
     doc_dir = 'images/content'
     images = [Document(content=f"{doc_dir}/{filename}", content_type="image") for filename in os.listdir('images/content/') ]
-    print('Images loaded........................')
+    print('Images loaded........................getImageSession')
     document_store.write_documents(images)
-    print('added to store........................',images)
+    print('added to store........................getImageSession',images)
     #print(images)
     retriever_text_to_image = MultiModalRetriever(
     document_store=document_store,
@@ -26,13 +25,39 @@ def getImage(query):
     query_embedding_model="sentence-transformers/clip-ViT-B-32",
     #query_embedding_model=sent_trans,
     query_type="text",
-    #document_embedding_models={"image": "sentence-transformers/clip-ViT-L-14"},)
     document_embedding_models={"image": "sentence-transformers/clip-ViT-B-32"},)
-    print('Retriver ........................')
+    print('Retriver ........................getImageSession')
     document_store.update_embeddings(retriever=retriever_text_to_image)
-    print('update embedding ........................')
+    print('update embedding ........................getImageSession')
     pipeline = Pipeline()
     pipeline.add_node(component=retriever_text_to_image, name="retriever_text_to_image", inputs=["Query"])
+    
+    return pipeline
+
+def getImage(query):
+    # print(query,'........................')
+    # sent_trans = 'sentence-transformers/nli-bert-base'
+    # document_store = InMemoryDocumentStore(embedding_dim=512)
+    # doc_dir = 'images/content'
+    # images = [Document(content=f"{doc_dir}/{filename}", content_type="image") for filename in os.listdir('images/content/') ]
+    # print('Images loaded........................')
+    # document_store.write_documents(images)
+    # print('added to store........................',images)
+    # #print(images)
+    # retriever_text_to_image = MultiModalRetriever(
+    # document_store=document_store,
+    # #query_embedding_model="sentence-transformers/clip-ViT-L-14",
+    # query_embedding_model="sentence-transformers/clip-ViT-B-32",
+    # #query_embedding_model=sent_trans,
+    # query_type="text",
+    # #document_embedding_models={"image": "sentence-transformers/clip-ViT-L-14"},)
+    # document_embedding_models={"image": "sentence-transformers/clip-ViT-B-32"},)
+    # print('Retriver ........................')
+    # document_store.update_embeddings(retriever=retriever_text_to_image)
+    # print('update embedding ........................')
+    # pipeline = Pipeline()
+    # pipeline.add_node(component=retriever_text_to_image, name="retriever_text_to_image", inputs=["Query"])
+    pipeline = getImageSessionpipeline()
     results = pipeline.run(query=query, params={"retriever_text_to_image": {"top_k": 1}})
     results = sorted(results["documents"], key=lambda d: d.score, reverse=True)
 
@@ -43,17 +68,6 @@ def getImage(query):
     # print(scores)
 
     return images_array,scores
-#     for ima, score in zip(images_array, scores):
-#         im = Image.open(ima)
-#         img_with_border = ImageOps.expand(im, border=20, fill="white")
 
-#         img = ImageDraw.Draw(img_with_border)
-#         img.text((20, 0), f"Score: {score},    Path: {ima}", fill=(0, 0, 0))
-
-#         bio = BytesIO()
-#         img_with_border.save(bio, format="png")   
-#         #img1display = display(IPImage(bio.getvalue(), format="png",width = 200))
-        
-#         return display(IPImage(bio.getvalue(), format="png",width = 200))
 
     
