41500657a922dfc5e6866f20ec83200aeae01767
venkatkp50 <95966437+venkatkp50@users.noreply.github.com>
2023-07-03 17:45:50 +0530

    Update multimodalReader.py

--- multimodalReader.py
+++ multimodalReader.py
@@ -52,7 +52,7 @@ def getImage(query):
     #query_embedding_model=sent_trans,
     query_type="text",
     document_embedding_models={"image": "sentence-transformers/clip-ViT-L-14"},)
-    3document_embedding_models={"image": "sentence-transformers/clip-ViT-B-32"},)
+    document_embedding_models={"image": "sentence-transformers/clip-ViT-B-32"},)
     st.write('Retriver ........................')
     document_store.update_embeddings(retriever=retriever_text_to_image)
     st.write('update embedding ........................')
