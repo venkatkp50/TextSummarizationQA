df5ce2de0613abdc9cf6cad46867331f42fa8a72
venkatkp50 <95966437+venkatkp50@users.noreply.github.com>
2023-07-03 16:23:48 +0530

    Update multimodalReader.py

--- multimodalReader.py
+++ multimodalReader.py
@@ -36,14 +36,14 @@ def getImageSessionpipeline():
     return pipeline
 
 def getImage(query):
-    print(query,'........................')
+    st.write(query,'........................')
     sent_trans = 'sentence-transformers/nli-bert-base'
     document_store = InMemoryDocumentStore(embedding_dim=512)
     doc_dir = 'images/content'
     images = [Document(content=f"{doc_dir}/{filename}", content_type="image") for filename in os.listdir('images/content/') ]
-    print('Images loaded........................')
+    st.write('Images loaded........................')
     document_store.write_documents(images)
-    print('added to store........................',images)
+    st.write('added to store........................',images)
     #print(images)
     retriever_text_to_image = MultiModalRetriever(
     document_store=document_store,
@@ -53,9 +53,9 @@ def getImage(query):
     query_type="text",
     #document_embedding_models={"image": "sentence-transformers/clip-ViT-L-14"},)
     document_embedding_models={"image": "sentence-transformers/clip-ViT-B-32"},)
-    print('Retriver ........................')
+    st.write('Retriver ........................')
     document_store.update_embeddings(retriever=retriever_text_to_image)
-    print('update embedding ........................')
+    st.write('update embedding ........................')
     pipeline = Pipeline()
     pipeline.add_node(component=retriever_text_to_image, name="retriever_text_to_image", inputs=["Query"])
     # pipeline = getImageSessionpipeline()
